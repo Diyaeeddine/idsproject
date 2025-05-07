adc6e323c45c48340f5a9920e8ca73a826b45b42
Bahae Eddine <bahaeeg@gmail.com>
2025-05-07 15:24:59 +0100

    affectation demande

--- resources/views/admin/demandes/add-demande.blade.php
+++ resources/views/admin/demandes/add-demande.blade.php
@@ -28,14 +28,12 @@
                         <input type="text" name="name" id="name" value='{{$user->name}}' required hidden>
                         <input type="text" name="email" id="email" value='{{$user->email}}' required hidden>
                         
-                        <!-- Type de demande -->
                         <div>
                             <x-input-label for="typeDemande" :value="__('Type de demande')" />
                             <x-text-input id="typeDemande" class="block mt-1 w-full" type="text" name="typeDemande" :value="old('typeDemande')" required autofocus />
                             <x-input-error :messages="$errors->get('typeDemande')" class="mt-2" />
                         </div>
 
-                        <!-- Description de la demande -->
                         <div class="mt-4">
                             <x-input-label for="descDemande" :value="__('Description de la demande')" />
                             <textarea id="descDemande" 
@@ -46,14 +44,12 @@
                             <x-input-error :messages="$errors->get('descDemande')" class="mt-2" />
                         </div>
 
-                        <!-- Justification -->
                         <div class="mt-4">
                             <x-input-label for="justDemande" :value="__('Justification')" />
                             <x-text-input id="justDemande" class="block mt-1 w-full" type="text" name="justDemande" :value="old('justDemande')" required />
                             <x-input-error :messages="$errors->get('justDemande')" class="mt-2" />
                         </div>
 
-                        <!-- Durée -->
                         <div class="mt-4">
                             <x-input-label for="duree" :value="__('Durée')" />
                             <select id="duree" name="duree" 
@@ -64,7 +60,6 @@
                             <x-input-error :messages="$errors->get('duree')" class="mt-2" />
                         </div>
 
-                        <!-- Urgence -->
                         <div class="mt-4">
                             <x-input-label for="urgence" :value="__('Urgence')" />
                             <select id="urgence" name="urgence" 
